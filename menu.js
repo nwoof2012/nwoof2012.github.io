f1443cd9fada986ed47722845744bc43a1fc7a3d
Nathan Daniel Cole Cyr <nwcole2012@gmail.com>
2023-06-21 21:03:13 -0400

    Update menu.js
    Removed unnecessary code

--- menu.js
+++ menu.js
@@ -1,18 +1,8 @@
-/*function toggleMenu() {
-    var menuObject = document.getElementById("nav");
-    if(menuObject.style.display = "none") {
-        menuObject.style.display = "block";
-    } else {
-        menuObject.style.display = "none";
-    }
-}*/
-
 const button = document.getElementsByClassName('menu-mobile-btn')[0];
 var menuObject = document.querySelector("nav");
 menuObject.style.top = '-100vh';
 var container = document.querySelector("html");
 button.addEventListener("click", function() {
-    //do your stuff 
     if (menuObject.style.top != '-150vh') {
         menuObject.style.top = '-150vh';
         container.classList.remove("scroll-hide");
@@ -20,5 +10,4 @@ button.addEventListener("click", function() {
         menuObject.style.top = 0;
         container.classList.add("scroll-hide");
     }
-    console.log("wof");
 });
