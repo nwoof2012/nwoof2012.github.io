4c3cf248c429b570113577673a12db0e4025e8d4
Nathan Daniel Cole Cyr <nwcole2012@gmail.com>
2024-12-25 15:48:28 -0500

    Updated Theme
    - Added Dark/Light Mode
    - Fixed some issues with Mobile Menu
    - Fixed Shader System Requirements on mobile

--- menu.js
+++ menu.js
@@ -1,13 +1,67 @@
-const button = document.getElementsByClassName('menu-mobile-btn')[0];
+var button = document.getElementsByClassName('menu-mobile-btn')[0];
 var menuObject = document.querySelector("nav");
-menuObject.style.top = '-100vh';
+var menuParent = document.getElementsByClassName('nav-parent')[0];
+var themeButton = document.getElementsByClassName('theme-button')[0];
 var container = document.querySelector("html");
+
+var menuToggle = false;
+
 button.addEventListener("click", function() {
-    if (menuObject.style.top != '-150vh') {
+    if (menuToggle) {
         menuObject.style.top = '-150vh';
+        menuParent.style.top = '-150vh';
+        themeButton.style.top = '-100vh';
         container.classList.remove("scroll-hide");
+        button.classList.remove("invert");
+        menuToggle = false;
     } else {
         menuObject.style.top = 0;
+        menuParent.style.top = 0;
+        themeButton.style.top = '65px';
         container.classList.add("scroll-hide");
+        button.classList.add("invert");
+        menuToggle = true;
     }
 });
+
+
+var colorScheme = 0;
+
+const body = document.querySelector("body");
+
+if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    colorScheme = 1;
+} else {
+    colorScheme = 0;
+}
+
+setCurrentTheme();
+
+themeButton.addEventListener("click", event => {
+    if(colorScheme == 1) {
+        colorScheme = 0;
+    } else {
+        colorScheme = 1;
+    }
+    setCurrentTheme();
+});
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const newColorScheme = event.matches ? "dark" : "light";
+
+    if(newColorScheme == "dark") colorScheme = 1;
+
+    if (newColorScheme == "light") colorScheme = 0;
+
+    setCurrentTheme();
+});
+
+function setCurrentTheme() {
+    if (colorScheme == 1) {
+        body.classList.remove("light");
+        body.classList.add("dark");
+    } else {
+        body.classList.remove("dark");
+        body.classList.add("light");
+    }
+}
