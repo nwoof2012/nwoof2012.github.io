7ca32b9d5b30230355fee9baa23ed489a2ce7b9c
Nathan Daniel Cole Cyr <nwcole2012@gmail.com>
2025-01-16 11:40:02 -0500

    Update ytfeed.js

--- ytfeed.js
+++ ytfeed.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $.getJSON("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${getSearchTerm()}&key=${YOUTUBE_TOKEN}`", function (data) {
+    $.getJSON("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${getSearchTerm()}&key=${AIzaSyDr0b4v9ASGPvRMpGN5OTvXlDNiB45mMMg}`", function (data) {
         var dataContainer = $("#videos ul");
         $.each(data.data.items, function(i, val) {
             if(typeof(val.player) !== 'undefined' && typeof(val.title) !== 'undefined') {
