ff54469942850bcae06921a654e3a1fb4ab430eb
Nathan Daniel Cole Cyr <nwcole2012@gmail.com>
2025-01-16 11:30:31 -0500

    Added YouTube Page

---
+++ ytfeed.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $.getJSON("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${getSearchTerm()}&key=${YOUTUBE_TOKEN}`", function (data) {
+        var dataContainer = $("#videos ul");
+        $.each(data.data.items, function(i, val) {
+            if(typeof(val.player) !== 'undefined' && typeof(val.title) !== 'undefined') {
+                dataContainer.append("<li><a href=" + val.player.default+" target='_blank'>"+val.title+"</a></li>");
+            }
+        });
+    });
+});
