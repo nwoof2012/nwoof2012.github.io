51e542e96d28d0744731171013688f5fbfa67e36
Nathan Daniel Cole Cyr <nwcole2012@gmail.com>
2025-04-29 12:12:07 -0400

    Overhauled Bluesky Widget

---
+++ bsky.js
@@ -0,0 +1,4 @@
+var widget = document.getElementsByClassName('bsky-widget')[0];
+var profileBackground = document.getElementsByClassName('profile-background')[0];
+var profileFollowers = document.getElementsByClassName('profile-followers')[0];
+var profileFollowing = document.getElementsByClassName('profile-following')[0];
